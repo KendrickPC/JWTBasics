1f04c337f8c5d11437af966d5ea66aa429041e6d
Kenneth Chang <kennethchang@kenneths-air.lan>
2022-12-03 19:31:46 -0800

    JWT setup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # JWTBasics
 
 ##### If a valid token is present in the request, the user can access specific info/data.
+
+##### JSON web token is just a way of exchanging data between two parties.
+
+[JWT](https://jwt.io/)
+
+
--- controllers/main.js
+++ controllers/main.js
@@ -4,12 +4,13 @@
 
 // setup authentication so only teh request with JWT can access the dashboard.
 
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
   const {username, password} = req.body
 
-  // mongo
+  // mongooose validations
   // Joi
   // if/else check in the controller
 
@@ -17,13 +18,18 @@ const login = async (req, res) => {
     throw new CustomAPIError('Plaese provide your email and password', 400)
   }
 
+  // Dummy id usually obtained from a database.
+  const id = new Date().getDate()
+  // In production, use long & complex & unhackable string values!
+  const token = jwt.sign({id, username}, )
+
   console.log(username, password);
   res.send('Login/Register/Signup Route')
 }
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100)
-  res.status(200).json({msg: 'Hello Kenjamin Button', secret: `Authorized data, yoru lucky number is ${luckyNumber}`})
+  res.status(200).json({msg: 'Hello Kenjamin Button', secret: `Authorized data, your lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
